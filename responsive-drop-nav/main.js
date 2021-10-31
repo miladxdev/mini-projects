7dd52485064cfcd70d4ec4c276d9efb75476a2f2
Milad <miladxgh@outlook.com>
2021-10-31 13:31:19 +0330

    first commit

---
+++ responsive-drop-nav/main.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+  $("nav ul li > a:not(:only-child)").click(function (e) {
+    $(this).siblings(".nav-dropdown").animate({
+      height: "toggle",
+    });
+
+    $(".nav-dropdown").not($(this).siblings()).hide();
+    e.stopPropagation();
+  });
+
+  $("html").click(function () {
+    $(".nav-dropdown").hide();
+  });
+
+  $("#nav-toggle").on("click", function () {
+    this.classList.toggle("active");
+  });
+
+  $("#nav-toggle").click(function () {
+    $("nav ul").toggle();
+  });
+});
